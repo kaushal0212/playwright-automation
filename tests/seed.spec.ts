bccc7cee39eb30485728ce0cf9d947e7ca0b76bb
Kaushal <kaushal177@gmail.com>
2026-09-19 22:22:29 -0500

    Added new console log

--- tests/seed.spec.ts
+++ tests/seed.spec.ts
@@ -3,5 +3,6 @@ import { test, expect } from '@playwright/test';
 test.describe('Test group', () => {
   test('seed', async ({ page }) => {
     // generate code here.
+    console.log("Hello Test")
   });
 });
