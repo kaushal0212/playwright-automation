66584966de6f050ce9085063088b3bb8be5a1402
Kaushal <kaushal177@gmail.com>
2026-09-21 09:42:38 -0500

    Updated script to visible not click

--- tests/github.spec.ts
+++ tests/github.spec.ts
@@ -5,10 +5,14 @@ import { test, expect } from '@playwright/test';
 test('Login test', async ({ page }) => {
     // await page.pause()
     await page.goto('https://github.com/login');
-   
+   await expect(page.getByRole('heading',{name: "Sign in to GitHub"})).toBeVisible();
     await page.getByLabel("Username or email address").fill("kaushal")
     await page.getByLabel("Password").fill("TEST")
     // await page.getByRole('button', { name: 'Sign in' }).click();
 //    await page.getByRole('button', { name: 'Sign in' ,exact:true}).click();
-    await page.getByRole('button',{name:'Sign in',exact:true}).click();
+    await expect(page.getByRole('button',{name:'Sign in',exact:true})).toBeVisible();
+    await expect(page.getByRole('link',{name:"Forgot password?"})).toBeVisible();
+    await expect(page.getByRole('link',{name : "Create an account"})).toBeVisible();
+    await expect(page.getByRole('button',{name: "Sign in with a passkey"})).toBeVisible();
+
 });
